04df3e641ee595850136fc9067377ffc78f30fa9
Ash727 <32463254+Ash727@users.noreply.github.com>
2022-08-05 15:51:16 -0400

    Initial commit

---
+++ Assets/Scripts/GameItems/Player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Player
+{
+	private int _userId;
+	private string _name;
+	private int _coins;
+	private int _betAmt;
+
+	public Player(Hashtable playerData)
+	{
+		_userId = (int)playerData["userId"];
+		_name = playerData["name"].ToString (); 
+		_coins = (int)playerData["coins"];
+		_betAmt = (int)playerData["bet"];
+	}
+	
+	public int GetUserId()
+	{
+		return _userId;
+	}
+	
+	public string GetName()
+	{
+		return _name;
+	}
+
+	public int GetCoins()
+	{
+		return _coins;
+	}
+
+	public void ChangeCoinAmount(int amount)
+	{
+		_coins += amount;
+	}
+
+    public int GetBet ()
+    {
+		return _betAmt;
+    }
+}
